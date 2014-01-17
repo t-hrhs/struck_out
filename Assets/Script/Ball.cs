3ee771f5c30fde2abe674efe1263cf2273bab7ae
t-hrhs <hirohashi.t.0204@gmail.com>
2014-01-18 00:49:56 +0900

    20140108作業done

--- Assets/Script/Ball.cs
+++ Assets/Script/Ball.cs
@@ -10,6 +10,13 @@ public class Ball : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		// start to touch
+		if (Input.GetMouseButtonDown(0)) {
+			Ray clkRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		}
+		else if (Input.GetMouseButtonUp(0)) {
+
+		}
 	}
 }
