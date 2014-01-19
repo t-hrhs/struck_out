2b2e59c224537d92423c6b2a345e21aef9b24a45
t-hrhs <hirohashi.t.0204@gmail.com>
2014-01-20 00:51:03 +0900

    config化を途中まで行った(2014.01.20)

--- Assets/Script/Config.cs
+++ Assets/Script/Config.cs
@@ -1,15 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Config : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+public static class Config {
+    //複数ステージ作る場合の考慮
+    //stage_number-1 because of array index
+    public static int stage_id = 0;
+    public static int panel_width_num = 3;
+    public static int[] panel_height_num = {3,3};
+    //panel_status
+    //0 : normal
+    //1以上 : 特殊パネル
+    public static int[,,] panel_status = {
+        {
+            {0,0,0},
+            {0,0,0},
+            {0,0,0}
+        },
+        {
+            {0,0,0},
+            {0,0,0},
+            {0,0,0}
+        },
+    };
 }
--- Assets/Script/Panel.cs
+++ Assets/Script/Panel.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Panel : MonoBehaviour {
     public static double judge_point_z = 13.5f;
+    public bool clear_flag = false;
 	// Use this for initialization
 	void Start () {
 	
@@ -13,6 +14,7 @@ public class Panel : MonoBehaviour {
 	    if (this.transform.position.z > (float)judge_point_z) {
             //constraintの初期化
             this.rigidbody.constraints = RigidbodyConstraints.None;
+            this.clear_flag = true;
             //colliderの判定をoffにする
             //this.collider.enabled = false;
         }
