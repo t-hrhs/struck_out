1a37750711bfe28d0d4560c965ae906fc0da5360
t-hrhs <hirohashi.t.0204@gmail.com>
2014-07-28 00:35:07 +0900

    適当なおまけ仕様

---
+++ Assets/Script/TestAnim.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestAnim : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Animator motion = GetComponent<Animator>();
+        AnimatorStateInfo state= motion.GetCurrentAnimatorStateInfo(0);
+
+        if(Input.GetKeyDown("space")){
+            motion.SetBool("Jump", true);
+        }
+
+        if(state.IsName("kick_22")){
+            motion.SetBool("Jump", false);
+        }
+	}
+}
