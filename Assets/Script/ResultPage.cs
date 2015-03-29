c15f29f6ad62765fc05eab8e645b76105ddf64b3
t-hrhs <hirohashi.t.0204@gmail.com>
2015-03-29 14:42:27 +0900

    change settings of button style

--- Assets/Script/ResultPage.cs
+++ Assets/Script/ResultPage.cs
@@ -2,8 +2,8 @@
 using System.Collections;
 
 public class ResultPage : MonoBehaviour {
-    public GUISkin style;
     public GUIStyle style_for_title;
+    public GUIStyle style_for_button;
     AudioSource audioSource;
 	// Use this for initialization
 	void Start () {
@@ -17,8 +17,8 @@ public class ResultPage : MonoBehaviour {
 	}
     void OnGUI () {
         //TopPageに飛ぶ為のボタンを設置する
-        GUI.skin = style;
         style_for_title.fontSize = (int)80 * Config.s_height/1080;
+        style_for_button.fontSize = (int)36 * Config.s_height/1080;
         Rect rect = new Rect(10,10,(float)Config.s_width * 0.9f ,(float)Config.s_height*0.1f);
         GUI.Label(rect,"Result",style_for_title);
         int x_offset = (int)(Config.s_width * 0.05);
@@ -31,7 +31,7 @@ public class ResultPage : MonoBehaviour {
             string temp = "残念!!\nクリアできず…";
             GUI.Label(new Rect(20,(float)Config.s_height * 0.2f,(float)Config.s_width * 0.9f, (float)Config.s_height*0.25f),temp,style_for_title);
         }
-        if (GUI.Button(new Rect((float)Config.s_width*0.05f,Config.s_height * 0.85f,(int)((Config.s_width-x_offset * 2) - interval),(int)((Config.s_height-y_offset * 2)/6 - interval)),"TopPage")) {
+        if (GUI.Button(new Rect((float)Config.s_width*0.05f,Config.s_height * 0.85f,(int)((Config.s_width-x_offset * 2) - interval),(int)((Config.s_height-y_offset * 2)/6 - interval)),"TopPage",style_for_button)) {
             //TopPageに飛ぶ
             Application.LoadLevel("TopPage");
         }
--- Assets/Script/TopPage.cs
+++ Assets/Script/TopPage.cs
@@ -2,8 +2,8 @@
 using System.Collections;
 
 public class TopPage : MonoBehaviour {
-    public GUISkin style;
     public GUIStyle style_for_title;
+    public GUIStyle style_for_button;
     public int user_clear_stage = 0;
 	// Use this for initialization
 	void Start () {
@@ -19,11 +19,9 @@ public class TopPage : MonoBehaviour {
 
     void OnGUI () {
         //説明画面に飛ぶ為のボタンを設置する
-        GUI.skin = style;
-        //ボタンの色の都合(あまり意味はない)
-        style_for_title = new GUIStyle();
         style_for_title.fontSize = (int)80 * Config.s_height/1080;
         style_for_title.normal.textColor = Color.white;
+        style_for_button.fontSize = (int)36 *  Config.s_height/1080;
         Rect rect = new Rect(10,10,(float)Config.s_width*0.9f,(float)Config.s_height * 0.12f);
         int x_offset = (int)(Config.s_width * 0.05);
         int y_offset = (int)(Config.s_height * 0.15);
@@ -32,7 +30,7 @@ public class TopPage : MonoBehaviour {
         int bt_size_x = (int)((Config.s_width-x_offset * 2) - interval);
         int bt_size_y = (int)((Config.s_height-y_offset * 2)/6 - interval);
         GUI.Label(rect,"Kick Target", style_for_title);
-        if (GUI.Button(new Rect(x_offset, tmp, bt_size_x, bt_size_y),"遊ぶ!!")) {
+        if (GUI.Button(new Rect(x_offset, tmp, bt_size_x, bt_size_y),"遊ぶ!!",style_for_button)) {
             Config.stage_id = 0;
             //Go to the 1st STG
             Application.LoadLevel("StageList");
