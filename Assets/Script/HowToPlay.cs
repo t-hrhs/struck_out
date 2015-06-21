dba39ee352548b3cc79ce449fe2ef97263c43aeb
t-hrhs <hirohashi.t.0204@gmail.com>
2015-06-21 22:45:52 +0900

    imple how to play including true img

--- Assets/Script/HowToPlay.cs
+++ Assets/Script/HowToPlay.cs
@@ -1,13 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class HowToPlay : MonoBehaviour {
-    public GUIStyle style_for_title;
-    public GUIStyle style_for_explain;
     public GUIStyle style_for_button;
+    public GUIStyle style_for_left_arrow;
+    public GUIStyle style_for_left_arrow_disable;
+    public GUIStyle style_for_right_arrow;
+    public GUIStyle style_for_right_arrow_disable;
+    public int page=0;
+    public int page_num = 2;
+    private static String how_to_bg_base_path = "Img/how_to_page0";
     // Use this for initialization
     void Start () {
-
+        page=0;
     }
 
     // Update is called once per frame
@@ -16,52 +22,57 @@ public class HowToPlay : MonoBehaviour {
     }
 
     void OnGUI () {
-        //説明画面に飛ぶ為のボタンを設置する
-        //TODO : Configに部分ごとのfontサイズを管理する
-        style_for_title.fontSize = (int)80 * Config.s_height/1080;
-        style_for_title.normal.textColor = Color.white;
         Rect rect = new Rect(10,10,(float)Config.s_width*0.9f,(float)Config.s_height * 0.12f);
-        GUI.Label(rect,"遊び方", style_for_title);
-        //説明文言の為のstyle
-        style_for_explain.fontSize = (int)36 * Config.s_height/1080;
-        style_for_explain.normal.textColor = Color.white;
-        Rect expl1 = new Rect(
-                10,
-                (float)Config.s_height * 0.13f,
-                (float)Config.s_width*0.9f,
-                (float)Config.s_height * 0.06f
+        Rect left_arrow = new Rect(
+            (int)(Config.s_width * 0.33),
+            (int)(Config.s_height * 0.89),
+            (int)(Config.s_width * 0.1),
+            (int)(Config.s_height * 0.05)
         );
-        GUI.Label(expl1, "1. まずはボールの蹴る位置を決めよう", style_for_explain);
-        Rect expl2 = new Rect(
-                (float)Config.s_width * 0.10f,
-                (float)Config.s_height * 0.35f,
-                (float)Config.s_width * 0.9f,
-                (float)Config.s_height * 0.12f
-        );
-        GUI.Label(expl2, "蹴る位置によってボールの軌道が\n変わるぞ", style_for_explain);
-        Rect expl3 = new Rect(
-                10,
-                (float)Config.s_height * 0.55f,
-                (float)Config.s_width*0.9f,
-                (float)Config.s_height * 0.06f
-        );
-        GUI.Label(expl3, "2. 簡単フリック操作でボールを蹴りだそう", style_for_explain);
-        Rect expl4 = new Rect(
-                (float)Config.s_width * 0.10f,
-                (float)Config.s_height * 0.75f,
-                (float)Config.s_width * 0.9f,
-                (float)Config.s_height * 0.12f
-        );
-        GUI.Label(expl4, "フリックの速さによって蹴る強さが\n変わるぞ", style_for_explain);
+        Rect right_arrow = new Rect(
+            (int)(Config.s_width * 0.58),
+            (int)(Config.s_height * 0.89),
+            (int)(Config.s_width * 0.1),
+            (int)(Config.s_height * 0.05)
+            );
         style_for_button.fontSize = (int)36 * Config.s_height/1080;
         int x_offset = (int)(Config.s_width * 0.05);
         int y_offset = (int)(Config.s_height * 0.15);
         int tmp = (int)(Config.s_height * 0.85);
         int interval = 5;
-        int bt_size_x = (int)((Config.s_width - x_offset * 2) - interval);
-        int bt_size_y = (int)((Config.s_height - y_offset * 2)/6 - interval);
+        int bt_size_x = (int)((Config.s_width - x_offset * 2)/4 - interval);
+        int bt_size_y = (int)((Config.s_height - y_offset * 2)/8 - interval);
         if(GUI.Button(new Rect(x_offset, tmp, bt_size_x, bt_size_y), "Top", style_for_button)) {
             Application.LoadLevel("TopPage");
+        }
+         //  pager
+        if (page  == 0) {
+          if(GUI.Button(left_arrow, "", style_for_left_arrow_disable)) {
+                //nothing to do
+           }
+        } else {
+            if(GUI.Button(left_arrow, "", style_for_left_arrow)) {
+                page--;
+                GameObject how_to_bg = GameObject.Find ("HowToBg");
+                String next_bg_path = how_to_bg_base_path + (page + 1);
+                Texture2D tex = (Texture2D) Resources.Load(next_bg_path, typeof(Texture2D));
+                HowToBg how_to_bg_obj = how_to_bg.GetComponent<HowToBg>();
+                how_to_bg_obj.GetComponent<Renderer>().material.mainTexture = tex;
+            }
+        }
+        if (page + 1 == page_num) {
+            if(GUI.Button(right_arrow, "", style_for_right_arrow_disable)) {
+                 //nothing to do
+            }
+        } else {
+            if(GUI.Button(right_arrow, "", style_for_right_arrow)) {
+                page++;
+                GameObject how_to_bg = GameObject.Find ("HowToBg");
+                String next_bg_path = how_to_bg_base_path + (page + 1);
+                Texture2D tex = (Texture2D) Resources.Load(next_bg_path, typeof(Texture2D));
+                HowToBg how_to_bg_obj = how_to_bg.GetComponent<HowToBg>();
+                how_to_bg_obj.GetComponent<Renderer>().material.mainTexture = tex;
+            }
         }
     }
 }
