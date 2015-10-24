001bd57957f87d79fb3b7822a82bdd40f3c77106
t-hrhs <hirohashi.t.0204@gmail.com>
2015-10-24 23:13:47 +0900

    modify indent

--- Assets/Script/TopPage.cs
+++ Assets/Script/TopPage.cs
@@ -19,20 +19,20 @@ public class TopPage : MonoBehaviour {
 
 	}
 
-    void OnGUI () {
-        //説明画面に飛ぶ為のボタンを設置する
-        style_for_button.fontSize = (int)36 *  Config.s_height/1080;
-        Rect rect = new Rect(10,10,(float)Config.s_width*0.9f,(float)Config.s_height * 0.12f);
-        float bt_x_offset = Config.s_width  *  0.05f;
-        float bt_y_offset = Config.s_height  *  0.85f;
-        float bt_size_x = Config.s_width  * ((0.9f-0.05f*(2-1))/2);
-        float bt_size_y = Config.s_height * 0.05f;
-        if (GUI.Button(new Rect(bt_x_offset, bt_y_offset, bt_size_x, bt_size_y),"遊び方",style_for_button)) {
-            Application.LoadLevel("HowToPlay");
-        }
-        if (GUI.Button(new Rect( bt_x_offset * 2 + bt_size_x, bt_y_offset, bt_size_x, bt_size_y),"遊ぶ!!",style_for_button)) {
-            Config.stage_id = 0;
-            Application.LoadLevel("StageList");
-        }
+  void OnGUI () {
+    //説明画面に飛ぶ為のボタンを設置する
+    style_for_button.fontSize = (int)36 *  Config.s_height/1080;
+    Rect rect = new Rect(10,10,(float)Config.s_width*0.9f,(float)Config.s_height * 0.12f);
+    float bt_x_offset = Config.s_width  *  0.05f;
+    float bt_y_offset = Config.s_height  *  0.85f;
+    float bt_size_x = Config.s_width  * ((0.9f-0.05f*(2-1))/2);
+    float bt_size_y = Config.s_height * 0.05f;
+    if (GUI.Button(new Rect(bt_x_offset, bt_y_offset, bt_size_x, bt_size_y),"遊び方",style_for_button)) {
+      Application.LoadLevel("HowToPlay");
     }
+    if (GUI.Button(new Rect( bt_x_offset * 2 + bt_size_x, bt_y_offset, bt_size_x, bt_size_y),"遊ぶ!!",style_for_button)) {
+      Config.stage_id = 0;
+      Application.LoadLevel("StageList");
+    }
+  }
 }
