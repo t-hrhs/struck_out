02c8f3cd1e291f8432760694c3ce0543ac6b746d
t-hrhs <hirohashi.t.0204@gmail.com>
2015-10-24 23:37:35 +0900

    add btn on top page

--- Assets/Script/TopPage.cs
+++ Assets/Script/TopPage.cs
@@ -16,23 +16,29 @@ public class TopPage : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-
+    if(Input.GetMouseButtonDown(0)) {
+      GameObject touched_object = get_touch_object();
+      if (touched_object.tag == "how_to_btn") {
+        Application.LoadLevel("HowToPlay");
+      } else if (touched_object.tag == "start_btn") {
+        Config.stage_id = 0;
+        Application.LoadLevel("StageList");
+      }
+    }
 	}
 
-  void OnGUI () {
-    //説明画面に飛ぶ為のボタンを設置する
-    style_for_button.fontSize = (int)36 *  Config.s_height/1080;
-    Rect rect = new Rect(10,10,(float)Config.s_width*0.9f,(float)Config.s_height * 0.12f);
-    float bt_x_offset = Config.s_width  *  0.05f;
-    float bt_y_offset = Config.s_height  *  0.85f;
-    float bt_size_x = Config.s_width  * ((0.9f-0.05f*(2-1))/2);
-    float bt_size_y = Config.s_height * 0.05f;
-    if (GUI.Button(new Rect(bt_x_offset, bt_y_offset, bt_size_x, bt_size_y),"遊び方",style_for_button)) {
-      Application.LoadLevel("HowToPlay");
+  //タッチしたobjectのタグを返す
+  //TODO : Utilに持っていきたい
+  GameObject get_touch_object() {
+    //マウスカーソルからのRay発射
+    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    RaycastHit hit;
+    // tagのついているobjectをタッチした場合
+    if (Physics.Raycast(ray, out hit)) {
+      if (hit.collider.gameObject.tag != "") {
+        return hit.collider.gameObject;
+      }
     }
-    if (GUI.Button(new Rect( bt_x_offset * 2 + bt_size_x, bt_y_offset, bt_size_x, bt_size_y),"遊ぶ!!",style_for_button)) {
-      Config.stage_id = 0;
-      Application.LoadLevel("StageList");
-    }
+    return null;
   }
 }
